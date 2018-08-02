a47908dcd9deb519e4185f8f2b76dc6156bdd814
a200411044 <simonbaby0711@gmail.com>
2018-08-02 15:13:40 +0800

    8/2

--- players.py
+++ players.py
@@ -2,3 +2,14 @@
 # -*- coding:utf-8 -*-
 # Author:Simon Law
 
+players = ['simon', 'peter', 'tom', 'amy', 'tim']
+'''
+print(players[0:3])
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+print(players[-3:])
+'''
+print("Here are the first three players on my team:")
+for players in players[:3]:
+    print(players.title())
